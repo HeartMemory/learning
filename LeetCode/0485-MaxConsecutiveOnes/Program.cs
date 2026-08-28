2c730eec4854b25523ba1808703eee6503897f68
HeartMemory <q13077396039@163.com>
2026-08-28 23:59:34 +0800

    Day 08-28: [LeetCode] 加一(66) + 最大连续1的个数(485) AC，更新刷题索引与打卡进度

---
+++ LeetCode/0485-MaxConsecutiveOnes/Program.cs
@@ -0,0 +1,33 @@
+﻿Solution sol = new Solution();
+Console.WriteLine(sol.FindMaxConsecutiveOnes(new int[] { 1, 1, 0, 1, 1, 1 })); // 期望 3
+Console.WriteLine(sol.FindMaxConsecutiveOnes(new int[] { 0, 0, 1, 0 }));       // 期望 1
+Console.WriteLine(sol.FindMaxConsecutiveOnes(new int[] { 0, 0 }));             // 期望 0
+
+public class Solution {
+    public int FindMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int result = 0;
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] == 1)
+            {
+                max += 1;
+            }
+            else if(max >= nums.Length / 2)
+            {
+                return max;
+            }
+            else if(max > result)
+            {
+                result = max;
+                max = 0;
+            }else{max = 0;}
+
+        }
+        if(max > result)
+            {
+                result = max;
+            }
+        return result;
+    }
+}
