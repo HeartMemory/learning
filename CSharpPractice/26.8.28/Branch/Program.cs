61d7486533bc9b1e568fdc2ca5cb2959b12ea609
HeartMemory <q13077396039@163.com>
2026-08-28 17:53:24 +0800

    Day 08-28: Day3分支 - switch菜单计算器 + 除0保护

--- CSharpPractice/26.8.28/Branch/Program.cs
+++ CSharpPractice/26.8.28/Branch/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double answer;
+Console.WriteLine("请输入第一个数字:");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("请输入第二个数字:");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("1.加法 2.减法 3.乘法 4.除法 5.取余");
+Console.WriteLine("请输入数字选择");
+int n = Convert.ToInt32(Console.ReadLine());
+switch (n)
+{
+    case 1:answer = a + b;Console.WriteLine(a + "+" + b + "=" + answer);break;
+    case 2:answer = a - b;Console.WriteLine(a + "-" + b + "=" + answer);break;
+    case 3:answer = a * b;Console.WriteLine(a + "*" + b + "=" + answer);break;
+    case 4:if(b == 0)
+        {
+            Console.WriteLine("除数不能为0");
+        }else{answer = a / b;Console.WriteLine(a + "/" + b + "=" + answer);}
+        break;
+    case 5:answer = a % b;Console.WriteLine(a + "%" + b + "=" + answer);break;
+    default:Console.WriteLine("输入不合法");break;
+}
