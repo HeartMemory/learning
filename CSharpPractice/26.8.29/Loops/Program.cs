cded147000b37aa6d8b1b270f14739ba6831af4f
HeartMemory <q13077396039@163.com>
2026-08-29 18:44:29 +0800

    Day 08-29: [循环] Day5提前学 - for/while/do-while + 乘法表/数列求和/求和器三练习

---
+++ CSharpPractice/26.8.29/Loops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+
+int add = 0;
+int mid = 0;
+int result = 0;
+int sum = 0;
+for(int i = 1;i <= 100; i++)
+{
+    add +=i;
+}
+Console.WriteLine(add);
+
+for(int i = 1;i < 10; i++)
+{
+    for(int j = 1;j <= i; j++)
+    {
+        mid = i * j;
+        Console.Write(j + "*" + i + "=" + mid + "\t");
+    }
+    Console.Write("\n");
+}
+
+do
+{
+    sum = int.Parse(Console.ReadLine());
+    result += sum;
+}while(sum != 0);
+Console.WriteLine("数字总和是"+result);
