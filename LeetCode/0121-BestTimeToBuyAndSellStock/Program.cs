01277473e3b3cfa274eee4cb760dfcc7a562edbe
HeartMemory <q13077396039@163.com>
2026-08-29 20:24:43 +0800

    Day 08-29: [算法] LeetCode 121 错误版本存档 - 全局高低点思路有漏洞，详见错题本01

---
+++ LeetCode/0121-BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,39 @@
+﻿Solution sol = new Solution();
+Console.WriteLine(sol.MaxProfit(new int[] { 7, 1, 5, 3, 6, 4 })); // 期望 5（第2天买1，第5天卖6）
+Console.WriteLine(sol.MaxProfit(new int[] { 7, 6, 4, 3, 1 }));    // 期望 0（价格一直跌，不买）
+Console.WriteLine(sol.MaxProfit(new int[] { 1, 2 }));             // 期望 1
+Console.WriteLine(sol.MaxProfit(new int[] { 5, 6, 1, 2 }));    // 期望 1
+Console.WriteLine(sol.MaxProfit(new int[] { 3, 2, 6, 5, 0, 3 })); // 期望 4
+
+
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int max = prices.Length-1;  //价格相对高的那一天
+        int min = 0;  //价格相对小的那一天
+        int result = 0;  //利润
+        int may = 0;  //可能存在的更优
+        for(int i = prices.Length-1;i > 0; i--)
+        {
+            if(prices[i] > prices[i - 1] && prices[i] > prices[max])
+            {
+                max = i;
+            }
+            if(prices[i] < prices[i - 1] && prices[i] < prices[min])
+            {
+                min = i;
+                if(max > min)
+                {
+                    result = prices[max] - prices[min];
+                }
+            }
+        }
+        if(max > min)
+        {
+            may = prices[max] - prices[min];
+        }
+        if(may > result)
+        {
+            return may;
+        }else{return result;}
+    }
+}
