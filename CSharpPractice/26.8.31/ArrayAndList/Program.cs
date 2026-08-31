65cb91474854fd8bcdb06adc8c561eaa73c232c4
HeartMemory <q13077396039@163.com>
2026-08-31 20:05:25 +0800

    Day 08-31: [里程碑] 计算器循环菜单版(历史记录+输入验证+除0保护) + Day6数组/List练习 + Projects目录结构建立

---
+++ CSharpPractice/26.8.31/ArrayAndList/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] nums = new int[]{0,1,-3,-9,8,7,5};
+double max = nums[0];
+double min = nums[0];
+double mid = nums[0];
+double all = nums[0];
+for(int i = 1;i < nums.Length; i++)
+{
+    max = Math.Max(max,nums[i]);
+    min = Math.Min(min,nums[i]);
+    all += nums[i];
+}
+mid = all / nums.Length;
+Console.WriteLine($"最大值是{max} 最小值是{min} 平均数是{mid}");
+
+double grade = 0;
+max = 0;
+min = 100;
+mid = 0;
+all = 0;
+List<double> list = new List<double>();
+Console.WriteLine("输入非法输入自动退出");
+Console.WriteLine("请输入成绩");
+while(true)
+{
+    if(double.TryParse(Console.ReadLine(),out grade) && grade >= 0 && grade <= 100){
+        Console.WriteLine($"您输入了{grade}");
+    }else{break;}
+    list.Add(grade);
+    max = Math.Max(max,grade);
+    min = Math.Min(min,grade);
+    all += grade;
+}
+if(list.Count > 0){
+    mid = all / list.Count;
+    Console.WriteLine($"最大值是{max} 最小值是{min} 平均数是{mid}");
+}else{Console.WriteLine("未输入任何数字");}
---
+++ Projects/CSharp/Calculator/Program.cs
@@ -0,0 +1,88 @@
+﻿double answer;
+int n = 0;
+double a = 0;
+double b = 0;
+List<string> list = new List<string>();
+while(true)
+{
+    Console.WriteLine("0.退出 1.加法 2.减法 3.乘法 4.除法 5.取余 6.历史记录");
+    Console.WriteLine("请输入数字选择");
+    if(int.TryParse(Console.ReadLine(), out n))
+    {
+        if(n == 0)
+        {
+            break;
+        }
+        if(n != 6)
+        {
+            Console.WriteLine("请输入第一个数字:");
+            if(!double.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("输入非法，请输入数字");
+                if(!double.TryParse(Console.ReadLine(), out a))
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("请输入第二个数字:");
+            if(!double.TryParse(Console.ReadLine(), out b))
+            {
+                Console.WriteLine("输入非法，请输入数字");
+                if(!double.TryParse(Console.ReadLine(), out b))
+                {
+                    break;
+                }
+            }
+        }
+        switch (n)
+        {
+            case 1:
+                answer = a + b;Console.WriteLine(a + "+" + b + "=" + answer);
+                list.Add(a + "+" + b + "=" + answer);
+                break;
+            case 2:
+                answer = a - b;Console.WriteLine(a + "-" + b + "=" + answer);
+                list.Add(a + "-" + b + "=" + answer);
+                break;
+            case 3:
+                answer = a * b;Console.WriteLine(a + "*" + b + "=" + answer);
+                list.Add(a + "*" + b + "=" + answer);
+                break;
+            case 4:
+                if(b == 0)
+                {
+                    Console.WriteLine("除数不能为0");
+                }else
+                {
+                    answer = a / b;Console.WriteLine(a + "/" + b + "=" + answer);
+                    list.Add(a + "/" + b + "=" + answer);
+                }
+                break;
+            case 5:
+                answer = a % b;Console.WriteLine(a + "%" + b + "=" + answer);
+                list.Add(a + "%" + b + "=" + answer);
+            break;
+            case 6:
+                if(list.Count == 0)
+                {
+                    Console.WriteLine("未进行过计算");
+                }
+                else
+                {
+                    for(int i = 0;i < list.Count; i++)
+                        {
+                            Console.WriteLine(list[i]);
+                        }
+                }
+                break;
+            default:
+            Console.WriteLine("输入非法");
+            break;
+        }
+    }
+    Console.WriteLine("输入任意整数以继续使用计算机");
+    if(!int.TryParse(Console.ReadLine(),out _))
+    {
+        break;
+    }
+}
