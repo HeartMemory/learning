10b6fe0bbe9b11a79ed6d16fb13b9c3a5410915f
HeartMemory <q13077396039@163.com>
2026-09-01 16:28:05 +0800

    09-01: [算法] LeetCode 88 错误版本存档 - 双层循环漏第三条路，详见错题本04 + 118 杨辉三角 AC

---
+++ LeetCode/0088-MergeSortedArray/Program.cs
@@ -0,0 +1,50 @@
+﻿Solution sol = new Solution();
+int[] t1 = { 1, 2, 3, 0, 0, 0 };
+sol.Merge(t1, 3, new int[] { 2, 5, 6 }, 3);
+Console.WriteLine(string.Join(",", t1)); // 期望 1,2,2,3,5,6
+
+int[] t2 = { 1 };
+int[] t2b = { };
+sol.Merge(t2, 1, t2b, 0);
+Console.WriteLine(string.Join(",", t2)); // 期望 1
+
+int[] t3 = { 0 };
+sol.Merge(t3, 0, new int[] { 1 }, 1);
+Console.WriteLine(string.Join(",", t3)); // 期望 1（边界：nums1 实际元素为 0 个）
+
+int[] t4 = { 3, 0, 0 };
+sol.Merge(t4, 1, new int[] { 1, 2 }, 2);
+Console.WriteLine(string.Join(",", t4)); // 期望 1,2,3
+
+
+public class Solution {
+    public void Merge(int[] nums1, int m, int[] nums2, int n) {
+        if(m != 0)
+        {
+            for(int i = n - 1;i >= 0; i--)
+            {
+                for(int j = m - 1;j >= 0; j--)
+                {
+                    if(nums1[j] > nums2[i])
+                    {
+                        nums1[j+i+1] = nums1[j];
+                    }
+                    else
+                    {
+                        nums1[j+i+1] = nums2[i];
+                        break;
+                    }
+                }
+            }
+        }
+        else if(n !=0)
+        {
+            for(int i = 0;i < n; i++)
+            {
+                nums1[i] = nums2[i];
+            }
+        }
+        // TODO: nums1 的前 m 个是有效元素，后面预留了 n 个 0 的空位；
+        // 把 nums2 合并进去，结果仍要有序（原地修改，不返回值）
+    }
+}
---
+++ LeetCode/0118-PascalsTriangle/Program.cs
@@ -0,0 +1,32 @@
+﻿Solution sol = new Solution();
+foreach (var row in sol.Generate(5))
+    Console.WriteLine(string.Join(" ", row));
+// 期望：
+// 1
+// 1 1
+// 1 2 1
+// 1 3 3 1
+// 1 4 6 4 1
+
+public class Solution {
+    public IList<IList<int>> Generate(int numRows) {
+        IList<IList<int>> result = new  List<IList<int>>();
+        for(int i = 0;i < numRows; i++)
+        {
+            result.Add(new List<int>(new int[i + 1]));
+            result[i][0] = 1;
+            result[i][i] = 1;
+        }
+        for(int i = 2;i < numRows; i++)
+        {
+            for(int j = 1;j < i; j++)
+            {
+                result[i][j] = result[i-1][j] + result[i-1][j-1];
+            }
+        }
+        return result;
+        // TODO: 生成杨辉三角前 numRows 行
+        // 每行是一个 List<int>，整体用 List<List<int>> 装
+        // 规律：每行首尾是 1，中间元素 = 上一行相邻两数之和
+    }
+}
