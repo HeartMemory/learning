3e3bc09608d1e247d66bed382a32af7beb838660
HeartMemory <q13077396039@163.com>
2026-09-04 21:22:41 +0800

    09-04: [字符串] Split切分与空串处理/Replace替换原理(切缝模型)/查找计数用原文

---
+++ CSharpPractice/2026/09/04/SplitReplace/Program.cs
@@ -0,0 +1,20 @@
+﻿string s = "a   a b cd";
+string[] words = s.Split(' ');
+for(int i = 0;i < words.Length; i++)
+{
+    if(words[i] != "")
+    {
+        Console.WriteLine(words[i]);
+    }
+}
+
+Console.WriteLine("输入原文");
+s = Console.ReadLine();
+Console.WriteLine("输入查找词");
+string a = Console.ReadLine();
+Console.WriteLine("输入替换词");
+string b = Console.ReadLine();
+string r = s.Replace(a,b);
+Console.WriteLine(r);
+int times = s.Split(a).Length-1;
+Console.WriteLine($"替换了 {times} 处，结果：{r}");
