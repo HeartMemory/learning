ddc84af692c8437c5ef5b65678f8bde891daa16d
HeartMemory <q13077396039@163.com>
2026-09-06 20:50:01 +0800

    09-06: [复盘日] 13罗马数字 List戒断重写 - 硬编码减法搭档单遍版，零容器

--- LeetCode/0013-RomanToInteger/Program.cs
+++ LeetCode/0013-RomanToInteger/Program.cs
@@ -1,53 +1,81 @@
 ﻿Solution sol = new Solution();
 Console.WriteLine(sol.RomanToInt("III"));     // 期望 3
-Console.WriteLine(sol.RomanToInt("IX"));      // 期望 9（小值在大值左边 = 减）
-Console.WriteLine(sol.RomanToInt("LVIII"));   // 期望 58（L=50 V=5 III=3）
-Console.WriteLine(sol.RomanToInt("MCMXCIV")); // 期望 1994（M CM XC IV：大减小组合连环）
+Console.WriteLine(sol.RomanToInt("IX"));      // 期望 9
+Console.WriteLine(sol.RomanToInt("LVIII"));   // 期望 58
+Console.WriteLine(sol.RomanToInt("MCMXCIV")); // 期望 1994
 
 public class Solution {
     public int RomanToInt(string s) {
-        List<int> list = new List<int>();
-        for(int i = 0;i < s.Length; i++)
+        int answer = 0;
+        for(int i = 0;i < s.Length;i++)
         {
-            if(s[i] == 'I')
+            if(s[i] == 'M')
             {
-                list.Add(1);
-            }else if(s[i] =='V')
-            {
-                list.Add(5);
-            }else if(s[i] == 'X')
-            {
-                list.Add(10);
-            }else if(s[i] == 'L')
-            {
-                list.Add(50);
-            }else if(s[i] == 'C')
-            {
-                list.Add(100);
+                answer += 1000;
             }else if(s[i] == 'D')
             {
-                list.Add(500);
-            }else if(s[i] == 'M')
+                if(i+1 < s.Length && s[i+1] == 'M')
+                {
+                    answer -= 500;
+                }
+                else
+                {
+                    answer += 500;
+                }
+            }else if(s[i] == 'C')
             {
-                list.Add(1000);
-            }
-        }
-        int answer = list[list.Count-1];
-        for(int i = list.Count-2;i >= 0; i--)
-        {
-            if(list[i+1] > list[i])
+                if(i+1 < s.Length && (s[i+1] == 'D' || s[i+1] == 'M'))
+                {
+                    answer -= 100;
+                }
+                else
+                {
+                    answer += 100;
+                }
+            }else if(s[i] == 'L')
             {
-                answer -= list[i];
-            }
-            else
+                if(i+1 < s.Length && (s[i+1] == 'C' || s[i+1] == 'D' || s[i+1] == 'M'))
+                {
+                    answer -= 50;
+                }
+                else
+                {
+                    answer += 50;
+                }
+            }else if(s[i] == 'X')
             {
-                answer += list[i];
+                if(i+1 < s.Length && (s[i+1] == 'L' || s[i+1] == 'C' || s[i+1] == 'D' || s[i+1] == 'M'))
+                {
+                    answer -= 10;
+                }
+                else
+                {
+                    answer += 10;
+                }
+            }else if(s[i] == 'V')
+            {
+                if(i+1 < s.Length && (s[i+1] == 'X' || s[i+1] == 'L' || s[i+1] == 'C' || s[i+1] == 'D' || s[i+1] == 'M'))
+                {
+                    answer -= 5;
+                }
+                else
+                {
+                    answer += 5;
+                }
+            }else if(s[i] == 'I')
+            {
+                if(i+1 < s.Length && (s[i+1] == 'V' || s[i+1] == 'X' || s[i+1] == 'L' || s[i+1] == 'C' || s[i+1] == 'D' || s[i+1] == 'M'))
+                {
+                    answer -= 1;
+                }
+                else
+                {
+                    answer += 1;
+                }
             }
         }
         return answer;
-        // TODO: 罗马数字转整数
-        // 规则：I=1 V=5 X=10 L=50 C=100 D=500 M=1000
-        // 特殊：小值在大值左边时做减法（IV=4, IX=9, XL=40, XC=90, CD=400, CM=900）
-        // 思路提示：逐字符累加；如果当前值 < 右边值，就减它；否则加它
+        // List 戒断版：禁止任何 List，单遍循环 + 一个 answer 变量
+        // 思路：从左往右走，当前值 < 右边值 → 它是减数；否则是加数
     }
 }
