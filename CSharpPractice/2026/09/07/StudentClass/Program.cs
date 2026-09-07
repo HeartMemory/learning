ee3af752e4235e660bed144f1d7dc9a2bb81987e
HeartMemory <q13077396039@163.com>
2026-09-07 17:40:25 +0800

    09-07: [OOP第一课] Student类起步 - 类/构造函数/字段/方法/对象调用，双对象独立验证

---
+++ CSharpPractice/2026/09/07/StudentClass/Program.cs
@@ -0,0 +1,19 @@
+﻿Student student1 = new Student("张雪峰",30);
+Student student2 = new Student("牢大",24);
+student1.Introduce();
+student2.Introduce();
+
+public class Student
+{
+    public string Name;
+    public int Age;
+    public Student(string Name = "张三", int Age = 18)
+    {
+        this.Name = Name;
+        this.Age = Age;
+    }
+    public void Introduce()
+    {
+        Console.WriteLine($"大家好，我叫{Name}，今年{Age}岁");
+    }
+}
