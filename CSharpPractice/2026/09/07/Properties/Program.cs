5377f6e2ca248925143dea0d9169ebef8852ed33
HeartMemory <q13077396039@163.com>
2026-09-07 22:41:09 +0800

    09-07: [OOP] 属性入门 - 完整属性校验(Age/Name门卫)/计算属性(Introduce/Info)/构造走属性赋值

---
+++ CSharpPractice/2026/09/07/Properties/Program.cs
@@ -0,0 +1,39 @@
+﻿Student student1 = new Student("张雪峰",30);
+Student student2 = new Student("牢大",114514);
+Console.WriteLine(student1.Introduce);
+Console.WriteLine(student2.Introduce);
+
+public class Student
+{
+    private int _age;
+    public int Age
+    {
+        get{return _age;}
+        set
+        {
+            if(value >= 0 && value <= 120)
+            {
+                _age = value;
+            }else{Console.WriteLine("年龄不合法");}
+        }
+    }
+    private string _name;
+    public string Name
+    {
+        get{return _name;}
+        set
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                _name = value;
+            }
+        }
+    }
+
+    public Student(string Name = "张三", int Age = 18)
+    {
+        this.Name = Name;
+        this.Age = Age;
+    }
+    public string Introduce => $"大家好，我叫{Name}，今年{Age}岁";
+}
